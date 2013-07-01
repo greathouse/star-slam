152ec75bf9d0f1d646f571f1cde708aaf86e04b8
Robert Greathouse <greathouse.robert@gmail.com>
2013-06-30 21:14:21 -0500

    add more tests for config file scanning

--- Core/src/test/groovy/ScanProjectTest.groovy
+++ Core/src/test/groovy/ScanProjectTest.groovy
@@ -3,27 +3,39 @@ import javax.xml.ws.WebEndpoint;
 import starslam.RunScanArguments
 import starslam.ScanService
 import starslam.Session
+import java.nio.file.*
 
 
 class ScanProjectTest extends TestBase {
+	def S = System.getProperty("file.separator")
+	
+	private Path createProjectRoot(String name) {
+		return Files.createTempDirectory("SimpleProject")
+	}
+	
 	private void writeFile(String path, String contents) {
 		new File(path).withWriter { writer ->
 			writer.writeLine(contents)
 		}
 	}
 	
+	private void writeFile(String dir, String file, String contents) {
+		new File(dir.toString()).mkdirs()
+		new File(dir+'/'+file).withWriter { writer ->
+			writer.writeLine(contents)
+		}
+	}
+	
 	void test_scan_shouldSaveProjectAndScanAttributes() {
-		def samplePath = /\src\test\resources\SimpleProject/
-		def workingDir = System.getProperty("user.dir")
 		def args = new RunScanArguments() 
 		args.with {
 			projectName = "Test Project " + UUID.randomUUID().toString()
-			projectRoot = workingDir+samplePath
+			projectRoot = createProjectRoot("SimpleProject").toString()
 			configFilePattern = "*.config"
 			sqlFileDirectory = "SQL.Migration"
 		}
 		Session.sql = sql
-		writeFile(args.projectRoot+/\web.config/, "<xml><some><content/></some></xml>")
+		writeFile(args.projectRoot+/${S}web.config/, "<xml><some><content/></some></xml>")
 		
 		def scanService = new ScanService()
 		scanService.scan(args)
@@ -42,7 +54,7 @@ class ScanProjectTest extends TestBase {
 		assert latestScan.configFiles.size == 1
 		
 		def configFile = latestScan.configFiles[0]
-		assert configFile.name == args.projectRoot+/\web.config/
+		assert configFile.name == args.projectRoot+/${S}web.config/
 		assert configFile.md5 != null
 		assert configFile.isNew
 		assert configFile.hasChanged == false
@@ -50,18 +62,16 @@ class ScanProjectTest extends TestBase {
 	}
 	
 	void test_scan_withConfigFileChange_shouldIndicateNotNewAndHasChanged() {
-		def samplePath = /\src\test\resources\MutatingProject/
-		def workingDir = System.getProperty("user.dir")
 		def args = new RunScanArguments()
 		args.with {
 			projectName = "Test Project " + UUID.randomUUID().toString()
-			projectRoot = workingDir+samplePath
+			projectRoot = createProjectRoot("MutatingProject").toString()
 			configFilePattern = "*.config"
 			sqlFileDirectory = "SQL.Migration"
 		}
 		Session.sql = sql
 		
-		def pathToConfig = args.projectRoot+/\web.config/
+		def pathToConfig = args.projectRoot+/${S}web.config/
 		writeFile(pathToConfig, "Initial content")
 		
 		def scanService = new ScanService()
@@ -83,5 +93,58 @@ class ScanProjectTest extends TestBase {
 		writeFile(pathToConfig, "Updated Content")
 		scanService.scan(args)
 		assertConfigFile(false, true)
+	}
+	
+	void test_scan_shouldFindsConfigNLevelDeep() {
+		def args = new RunScanArguments()
+		args.with {
+			projectName = "Test Project " + UUID.randomUUID().toString()
+			projectRoot = createProjectRoot("MutatingProject").toString()
+			configFilePattern = "*.config"
+			sqlFileDirectory = "SQL.Migration"
+		}
+		Session.sql = sql
+
+		def pathToConfig = args.projectRoot+"${S}n-level${S}deep${S}"
+		def configFileName = 'web.config'
+		writeFile(pathToConfig, configFileName, "Initial content")
+
+		def scanService = new ScanService()
+		scanService.scan(args)
+		
+		def latestScan = scanService.latestScan(args.projectName)
+		assert latestScan != null
+		assert latestScan.configFiles != null
+		assert latestScan.configFiles.size == 1
+		def configFile = latestScan.configFiles[0]
+		assert configFile.name.contains(pathToConfig), "Expected Contians <${pathToConfig}>; Actual: <${configFile.name}>"
+		assert configFile.name.contains(configFileName), "Expected Contians <${configFileName}>; Actual Config File Name [${configFile.name}]"
+	}
+	
+	void test_scan_shouldFindConfigFilesWithDifferentExentions() {
+		def args = new RunScanArguments()
+		args.with {
+			projectName = "Test Project " + UUID.randomUUID().toString()
+			projectRoot = createProjectRoot("MutatingProject").toString()
+			configFilePattern = "*{.config,.xml}"
+			sqlFileDirectory = "SQL.Migration"
+		}
+		Session.sql = sql
+
+		def pathToConfig = args.projectRoot
+		def configFileName1 = 'web.config'
+		def configFileName2 = 'app.xml'
+		writeFile(pathToConfig, configFileName1, "Initial content")
+		writeFile(pathToConfig, configFileName2, "Initial content")
+		writeFile(pathToConfig, 'negative.txt', 'Negative text file. Should not be included in scan')
+
+		def scanService = new ScanService()
+		scanService.scan(args)
+		
+		def latestScan = scanService.latestScan(args.projectName)
+		assert latestScan != null
+		assert latestScan.configFiles != null
+		assert latestScan.configFiles.size == 2
+		println latestScan.configFiles.find { it.name.contains('.config') }
 	}
 }
