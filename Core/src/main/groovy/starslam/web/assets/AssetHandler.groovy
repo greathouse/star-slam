a00a95bae667b70b200e409b5d215ee08d9dd98d
Robert Greathouse <greathouse.robert@gmail.com>
2013-08-20 23:07:36 -0500

    started on some knockoutjs stuff

--- Core/src/main/groovy/starslam/web/assets/AssetHandler.groovy
+++ Core/src/main/groovy/starslam/web/assets/AssetHandler.groovy
@@ -3,11 +3,26 @@ package starslam.web.assets
 import org.ratpackframework.handling.Context
 import org.ratpackframework.handling.Handler
 
-class AssetHandler implements Handler { 
+class AssetHandler implements Handler {
+	private static final Map<String,String>MIME_TYPES = [
+		'html':'text/html'
+		, 'js':'application/javascript'
+		, 'css':'text/css'
+		, 'ico':'image/x-icon'
+	] 
 	@Override
 	public void handle(Context exchange) {
-		def asset = this.class.getResource(exchange.request.path)?.text
-		def context = exchange.request.path.endsWith('.css') ? "text/css" : "text/javascript"
-		exchange.response.send(context, asset)
+		def path = exchange.request.path?:'index.html'
+		def map = URLConnection.getFileNameMap()
+		def mimeType = path.substring(path.lastIndexOf('.')+1)
+		def asset = this.class.getResourceAsStream('/starslam/web/assets/'+path)?.text
+		if (asset == null) {
+			def e404 = this.class.getResourceAsStream('/starslam/web/assets/404.html')
+			def e404Text = e404.text
+			exchange.response.status 404
+			exchange.response.send('text/html',e404Text)
+			return
+		}
+		exchange.response.send(mimeType, asset)
 	}
 }
